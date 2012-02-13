79d79d9a4139514b0f4e25de79517e84b696963d
Mikael Piotrowski <mishakun@gmail.com>
2012-02-13 13:06:57 +0100

    Added git_ws variable to add whitespace as needed

--- prompt.sh
+++ prompt.sh
@@ -4,17 +4,21 @@ function find_git_branch {
       if [ -f "$dir/.git/HEAD" ]; then
          head=$(< "$dir/.git/HEAD")
          if [[ $head == ref:\ refs/heads/* ]]; then
-            git_branch=" (${head#*/*/})"
+            git_branch="(${head#*/*/})"
+            git_ws=' '
          elif [[ $head != '' ]]; then
-            git_branch=' (detached)'
+            git_branch='(detached)'
+            git_ws=' '
          else
-            git_branch=' (unknown)'
+            git_branch='(unknown)'
+            git_ws=' '
          fi
          return
       fi
       dir="../$dir"
    done
    git_branch=''
+   git_ws=''
 }
 
 PROMPT_COMMAND="find_git_branch; $PROMPT_COMMAND"
